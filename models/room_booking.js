b953bcbd0652b5fb0cfb4e594c668051da313efb
Asq Arrsl <asqarrsl@gmail.com>
2021-09-18 11:09:45 +0530

    Booking Rooms Models

---
+++ models/room_booking.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const roomBookingSchema = new Schema(
+  {
+    room: {
+      type: Schema.Types.ObjectId,
+      ref: "Room",
+    },
+    customer: {
+      type: Schema.Types.ObjectId,
+      ref: "Customer",
+    },
+    ammenities: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Amenities",
+      },
+    ],
+    facilities: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Facility",
+      },
+    ],
+    check_in: Date,
+    check_checkout: Date,
+    // image:[imageSchema],
+    status: {
+      type: String,
+      default: 1,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("RoomBooking", roomBookingSchema);
